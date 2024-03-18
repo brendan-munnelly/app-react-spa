8335d03fe2b662aa8d7e55dc142eef6d87c1ba6b
Brendan Munnelly <munnellyb@gmail.com>
2024-03-18 03:53:03 +0000

    fixed again RoutComponent.jsx

--- src/RootComponent.jsx
+++ src/RootComponent.jsx
@@ -1,4 +1,5 @@
-import { useNavigate, useEffect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
 import App from './App';
 
 const RootComponent = () => {
